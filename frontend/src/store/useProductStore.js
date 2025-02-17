185b6b533a1117ae633dbccd60126c792ca85615
samuel maveke <mavekepeter2022@gmail.com>
2025-02-17 16:34:05 +0300

    finished add product UI and functionalityh

--- frontend/src/store/useProductStore.js
+++ frontend/src/store/useProductStore.js
@@ -9,6 +9,37 @@ export const useproductStore = create((set,get)=>({
     products:[],
     loading:false,
     error:null,
+    
+    //form state
+    formData:{
+        name:"",
+        price:"",
+        image:"",
+    },
+    setFormData:(formData)=>set({formData}),
+    resetForm:()=>set({formData:{name:"",price:"",image:""}}),
+
+    addProduct:async(e)=>{
+        e.preventDefault();
+        set({loading:true});
+
+        try {
+            const {formData} = get();
+            await axios.post(`${BASE_URL}/api/products`, formData);
+            await get().fetchProducts();
+            get().resetForm();
+            toast.success("Product added succefully");
+            //close the model
+            document.getElementById("add_product_modal").close();
+
+        } catch (error) {
+            console.log("Error in the addProduct function",error);
+            toast.error("Something went wrong");    
+        }finally{
+            set({loading:false});
+        }
+    },
+    
     fetchProducts:async ()=>{
         set({loading:true});
         try {
